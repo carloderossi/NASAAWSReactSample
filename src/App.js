7c3d683694bdebbf55cc794c3cd0008344722cb7
Carlo De Rossi <cdr.ipad@hotmail.com>
2023-05-12 21:34:21 +0200

    https://github.com/carloderossi/NASAAWSReactSample.git

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+
+const App = () => {
+   const [nasaimgs, setNasaimgs] = useState([]);
+   useEffect(() => {
+      fetch('https://api.nasa.gov/planetary/apod?api_key=qHRNOGuPFkO0alZ1AfyaVx2Y5ypOhjFHp5weq8ol&count=1&hd=true')
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            setNasaimgs(data);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
+
+   return (
+    <div className="nasaimgs-container">
+       {nasaimgs.map((nasaimg) => {
+          return (
+             <div className="nasaimg-id" key={nasaimg.url}>
+                <h2 className="nasaimg-title">{nasaimg.title}</h2>
+                <img src={nasaimg.hdurl} className="imgcenter" alt='NASA' width='50%' />
+                <p className="nasaimg-explanation">{nasaimg.explanation}</p>
+                <p className="nasaimg-hdurl">{nasaimg.hdurl}</p>
+             </div>
+          );
+       })}
+    </div>
+    );
+ };
+ 
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +52,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
